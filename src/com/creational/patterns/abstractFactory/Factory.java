0e7275d5cd3c029a1c5fb8372711528344984ee6
SergeyPodgorny <serkonpod@gmail.com>
2022-03-17 14:36:29 +0300

    added abstract factory pattern

--- src/com/creational/patterns/abstractFactory/Factory.java
+++ src/com/creational/patterns/abstractFactory/Factory.java
@@ -3,13 +3,32 @@ package com.creational.patterns.abstractFactory;
 public class Factory {
 
 	
-	public Animal getInstance(String aimalType) {
+	public Animal createInstance(String animalType) {
+		
+			
+		if(animalType == "Cow") {
+			
+			return new Cow();
+		
+		} else if(animalType == "Dog") {
+			
+			return new Dog();
+						
+		} else if(animalType == "Cat") {
+			return new Cat();
+		} else {
+			
+			return null;
+		}
 		
 		
 		
-		return null;
 		
 		
+		
+		
+		
+	
 		
 		
 		
