1f78eb26e79517a79f55fba92116300b102a9472
SergeyPodgorny <serkonpod@gmail.com>
2022-03-28 14:08:28 +0300

    working on abstract factory pattern

--- src/com/creational/patterns/AbstractFactory/FactoryProvider.java
+++ src/com/creational/patterns/AbstractFactory/FactoryProvider.java
@@ -10,24 +10,27 @@ public class FactoryProvider implements AbstractFactory {
 		this.type = type;
 	}
 
-
+	AnimalFactory animalFactory = new AnimalFactory();
 	
-
+	
 
 	@Override
 	public Object create(String type) {
 		
-		AnimalFactory animalFactory = new AnimalFactory();
+		
 		
 		if (type.equalsIgnoreCase("Cat")) {
 			
+			animalFactory.create("Cat");
 			
+		} else if (type.equalsIgnoreCase("Dog")) {
+			animalFactory.create("Dog");
 			
-		}
+		} else if (type.equalsIgnoreCase("Cow")) {
+			animalFactory.create("Cow");
+			
+		} throw new AnimalNotFoundException("");
 		
-		
-		
-		return null;
 	}
 	
 	
